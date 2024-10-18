cec55373654e385f3a9ca50e8afd04586b3e94c5
Mathias Bösl <mathias.boesl@gmx.de>
2024-10-18 16:29:30 +0200

    update

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-podman run -it --name nginx-test --net test docker.io/library/nginx:1.27
+CONTAINER_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+podman run -d --name nginx-test --rm --net test -p 80:80 \
+    -v "$CONTAINER_ROOT/nginx:/etc/nginx" \
+    docker.io/library/nginx:1.27
