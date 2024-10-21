90feec232a89c48a2d5674d98d8057fcc09f41ce
Mathias Bösl <mathias.boesl@gmx.de>
2024-10-21 22:18:44 +0200

    restructure

--- run-nginx.sh
+++ run-nginx.sh
@@ -3,8 +3,8 @@
 CONTAINER_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 podman run -dt --name nginx-test --rm --net test -p 80:80 \
-    -v "${CONTAINER_ROOT}/nginx-sample-config/nginx.conf:/etc/nginx/nginx.conf:ro" \
-    -v "${CONTAINER_ROOT}/nginx-sample-config/default.conf:/etc/nginx/conf.d/default.conf:ro" \
-    -v "${CONTAINER_ROOT}/src/:/etc/nginx/github_auth/:ro" \
+    -v "${CONTAINER_ROOT}/nginx-sample-config/nginx.conf:/etc/nginx/nginx.conf" \
+    -v "${CONTAINER_ROOT}/nginx-sample-config/default.conf:/etc/nginx/conf.d/default.conf" \
+    -v "${CONTAINER_ROOT}/src/:/etc/nginx/github_auth/" \
     -v "${CONTAINER_ROOT}/var/log/nginx:/var/log/nginx" \
     docker.io/library/nginx:1.27
