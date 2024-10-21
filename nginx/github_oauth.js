7d1edc2b28ea76ba5e3f955e28defbba590714e2
Mathias Bösl <mathias.boesl@gmx.de>
2024-10-21 21:31:03 +0200

    handle unset Name; fix logging (remove loggin private data); some cleanup

--- nginx/github_oauth.js
+++ nginx/github_oauth.js
@@ -24,13 +24,16 @@ function get_kv(zone) {
 }
 
 function sync_requestToken(r, code) {
-  ngx.log(ngx.WARN, "[sync_requestToken]");
+  ngx.log(ngx.INFO, "[sync_requestToken]");
 
   r.subrequest("/_github_access_token", 'code=' + code,
     function (reply) {
-      ngx.log(ngx.WARN, "[sync_requestToken] reply");
-      if (reply.status !== 200)
-        return error('OAuth unexpected response from authorization server (HTTP ' + reply.status + '). ' + reply.responseBody, 500);
+      ngx.log(ngx.INFO, "[_github_access_token] reply");
+
+      if (reply.status !== 200) {
+        const error_text = 'OAuth unexpected response from authorization server (HTTP ' + reply.status + '). ';
+        return error(error_text, 500);
+      }
 
       try {
         const response = JSON.parse((reply.responseText));
@@ -39,8 +42,6 @@ function sync_requestToken(r, code) {
         if (!token) {
           return r.return(500, "Toke is missing in reply");
         }
-
-        ngx.log(ngx.WARN, "[sync_requestToken] " + JSON.stringify(token));
 
         r.headersOut['Set-Cookie'] = "token=" + token;
 
@@ -51,13 +52,13 @@ function sync_requestToken(r, code) {
           const request_uri = kv.get(state);
           kv.delete(state);
 
-          ngx.log(ngx.WARN, "[sync_requestToken] state: " + state + "=" + request_uri);
-
           r.return(302, request_uri);
         } else {
-          r.return(500, "State provided from Github is not known");
+          const error_text = "State provided from Github is not known";
+          r.return(500, error_text);
         }
       } catch (e) {
+        ngx.log(ngx.WARN, "[_github_access_token] exception " + e);
         r.return(500, e);
       }
     }
@@ -65,7 +66,7 @@ function sync_requestToken(r, code) {
 }
 
 function sync_login(r) {
-  ngx.log(ngx.WARN, "[sync_login] " + JSON.stringify(r))
+  ngx.log(ngx.WARN, "[sync_login]")
 
   if (r.args && r.args.code && typeof r.args.code === 'string') {
     sync_requestToken(r, r.args.code);
@@ -76,7 +77,7 @@ function sync_login(r) {
 
 function sync_authenticate(r) {
   if (r.variables.cookie_token) {
-    ngx.log(ngx.WARN, "[sync_authenticate] cookie token: " + r.variables.cookie_token);
+    ngx.log(ngx.WARN, "[sync_authenticate]");
 
     const kv = get_kv(r.variables['github_loggedin_zone_name']);
 
@@ -86,35 +87,33 @@ function sync_authenticate(r) {
     if (logged_in_val) {
       // we are storing the user name in as the value of the team+token combination. 
       // The user name is not dependant of the team (only of the token) but it helps reducing complexity (at the cost of ... logic).
-      const response = JSON.parse((logged_in_val));
-      r.headersOut['login'] = response.login;
-      r.headersOut['name'] = response.name;
+      const credentials = JSON.parse((logged_in_val));
+      r.headersOut['login'] = credentials.login;
+      r.headersOut['name'] = credentials.name;
 
       return r.return(200);
     } else {
       r.subrequest("/_github_user_info",
         function (reply) {
-          ngx.log(ngx.WARN, "[sync_authenticate][github_user_info] reply");
-          if (reply.status !== 200)
-            return error(r, 'OAuth unexpected response from authorization server (HTTP ' + reply.status + '). ' + reply.responseBody, reply.status);
+          ngx.log(ngx.INFO, "/_github_user_info reply");
+          if (reply.status !== 200) {
+            const error_test = 'OAuth unexpected response from authorization server (HTTP ' + reply.status + '). ';
+            return error(r, error_test, reply.status);
+          }
 
           const response = JSON.parse((reply.responseText));
           const login = response['login'];
-          const name = response['name'];
-
-          ngx.log(ngx.WARN, "[user_info] login: " + login + ", name: " + name);
+          const name = response['name'] ? response['name']: login;
 
           r.headersOut['login'] = login;
           r.headersOut['name'] = name;
 
           r.subrequest("/_github_team_membership", "login=" + login,
             function (reply) {
-              ngx.log(ngx.WARN, "[sync_authenticate][github_team_membership] reply");
+              ngx.log(ngx.WARN, "/_github_team_membership reply");
               if (reply.status === 200) {
                 let response = JSON.parse((reply.responseText));
                 let state = response['state'];
-
-                ngx.log(ngx.WARN, "[check_team_membership] state: " + state)
 
                 if (state === "active") {
                   kv.set(logged_in_key, JSON.stringify({
@@ -124,16 +123,20 @@ function sync_authenticate(r) {
 
                   r.return(200);
                 } else {
-                  r.return(403, "User is not a member of the team");
+                  const error_test = "User is not a member of the team";
+                  ngx.log(ngx.INFO, error_test);
+                  r.return(403, error_test);
                 }
               } else {
-                error(r, "Error checking team membership", reply.status);
+                const error_test = "Error checking team membership";
+                error(r, error_test, reply.status);
               }
             });
         }
       );
     }
   } else {
+    const error_test = "Missing Token";
     error(r, "Missing Token", 401);
   }
 }
@@ -145,8 +148,6 @@ function store_uri(r) {
   const random_string = Buffer.from(random).toString('base64url')
   const request_uri = r.variables['request_uri'];
   kv.set(random_string, request_uri);
-
-  ngx.log(ngx.WARN, "[store_uri] github state" + random_string + "=" + request_uri);
 
   return random_string;
 }
